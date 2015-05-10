c00b9cb30e8ed24665706e7e1772a3fad5261c6d
vitaly - git <vitaly@secthor.com>
2015-05-10 15:27:41 +0300

    Init - Webpack dev functional via gulp

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp");
+var gutil = require("gulp-util");
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+
+var webpackConfig = require("./webpack.config.js");
+
+gulp.task("default", ["webpack-dev-server"]);
+
+gulp.task("webpack-dev-server", function(callback) {
+    // modify some webpack config options
+    var myConfig = Object.create(webpackConfig);
+    myConfig.devtool = "eval";
+    myConfig.debug = true;
+
+    // Start a webpack-dev-server
+    new WebpackDevServer(webpack(myConfig), {
+        stats: {
+            colors: true
+        }
+    }).listen(8080, "localhost", function(err) {
+        if(err) throw new gutil.PluginError("webpack-dev-server", err);
+        gutil.log("[webpack-dev-server]", myConfig.output.publicPath);
+        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
+    });
+});
