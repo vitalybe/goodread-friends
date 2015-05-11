f7bf2b62a60726c032231b596f4db841ec8299ba
vitaly - git <vitaly@secthor.com>
2015-05-11 15:38:44 +0300

    Setting state

--- hello.jsx
+++ hello.jsx
@@ -1,7 +1,24 @@
 var React = require('react');
 module.exports = React.createClass({
     displayName: 'HelloReact',
+    getInitialState() {
+        return {yourName: ""};
+    },
+    onInputChange() {
+        var value = React.findDOMNode(this.refs.name).value;
+        this.setState({yourName: value});
+    },
     render: function () {
-        return <div>Hello React</div>
+        return (
+            <div>
+                <div>
+                    <span>Enter your name: </span>
+                    <input ref="name" onChange={this.onInputChange}></input>
+                </div>
+                <div>
+                    <span>Welcome: {this.state.yourName}</span>
+                </div>
+            </div>
+        )
     }
 });
